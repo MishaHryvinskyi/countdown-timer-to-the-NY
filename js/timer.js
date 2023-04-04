f745a287fde24a4a0443232e84fd75a7a17e5b6a
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-04-04 16:44:25 +0300

    add style and js

--- js/timer.js
+++ js/timer.js
@@ -8,16 +8,32 @@ const refs = {
     startBtn: document.getElementById("startBtn"),
 };
 
-// 2) Створюємо дату наступного Нового Року до якого будемо рахувати час.
-// 3) Отримати поточну дату
-// 4) Отримати різницю. Від дати Нового Року відняти поточну дату.
-// таким чином ми отримаємо кількість мілісекунд яка залишається до НР.
-// 5) З нашої різниці витягнути дні, години, хвилини і секунди.
-// 6) Показати результат на екрані
-// 7) запустити сет таймаут, який буде оновлювати час кожну секунду.
 
+
+
+
+// 2) Створюємо дату наступного Нового Року до якого будемо рахувати час.
 const countDownDate = new Date(`Jan 1, ${new Date().getFullYear() +1}`);
-console.log('countDownDate:', countDownDate);
-console.log(countDownDate); 
-const day = new Date();
-console.log(new Date());
+
+// 7) запустити сет таймаут, який буде оновлювати час кожну секунду.
+setInterval(countDownTimeToNY, 1000);
+
+function countDownTimeToNY () {
+    // 3) Отримати поточну дату
+    const now = new Date().getTime();
+
+    // 4) Отримати різницю. Від дати Нового Року відняти поточну дату.
+    // таким чином ми отримаємо кількість мілісекунд яка залишається до НР.
+    const diff = countDownDate - now;
+    
+    // 5) З нашої різниці витягнути дні, години, хвилини і секунди.
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(diff / (1000 * 60 * 60) % 24);
+    const minutes = Math.floor(diff / (1000 * 60) % 60);
+    const seconds = Math.floor(diff / (1000) % 60);
+
+    // 6) Показати результат на екрані
+    refs.timer.textContent = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+};
+
+countDownTimeToNY();
